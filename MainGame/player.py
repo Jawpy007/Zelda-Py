d09607de772ad995a51e0ca70c17ff420b1496d1
Joe6987 <123161542+Joe6987@users.noreply.github.com>
2025-01-30 20:03:36 +0100

    Update player.py

--- MainGame/player.py
+++ MainGame/player.py
@@ -6,3 +6,26 @@ class Player(pygame.sprite.Sprite):
 		super().__init__(groups)
 		self.image = pygame.Surface((TILESIZE,TILESIZE))
 		self.rect = self.image.get_rect(topleft = pos)
+
+		self.direction = pygame.math.Vector2()
+
+	def input(self):
+		keys = pygame.key.get_pressed()
+
+		# movement input
+		if keys[pygame.K_UP]:
+			self.direction.y = -1
+		elif keys[pygame.K_DOWN]:
+			self.direction.y = 1
+		else:
+			self.direction.y = 0
+
+		if keys[pygame.K_RIGHT]:
+			self.direction.x = 1
+		elif keys[pygame.K_LEFT]:
+			self.direction.x = -1
+		else:
+			self.direction.x = 0
+
+	def update(self):
+		self.input()
