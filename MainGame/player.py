b91b9fa882374903e424528acf81c3c213054299
Joe6987 <123161542+Joe6987@users.noreply.github.com>
2025-01-30 19:53:51 +0100

    Update player.py

--- MainGame/player.py
+++ MainGame/player.py
@@ -5,4 +5,4 @@ class Player(pygame.sprite.Sprite):
 	def __init__(self,pos,groups):
 		super().__init__(groups)
 		self.image = pygame.Surface((TILESIZE,TILESIZE))
-    self.rect = self.image.get_rect(topleft = pos)
+		self.rect = self.image.get_rect(topleft = pos)
